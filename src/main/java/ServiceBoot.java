100355f176e6464107a034e4834b5b430d3e3c42
jdelvinquier <jeremy.delvinquier@gmail.com>
2017-01-12 09:08:33 +0100

    Spring boot test

---
+++ src/main/java/ServiceBoot.java
@@ -0,0 +1,10 @@
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class ServiceBoot {
+ 
+       public static void main(String[] args) {
+            SpringApplication.run(ServiceBoot.class);
+        }
+     
+}
